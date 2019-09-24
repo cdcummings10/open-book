d204309389319676fb64dcf9d7d29a52fd7d41e0
ENRIQUE RIVERA <KIK.RDL3@GMAIL.COM>
2019-09-24 10:27:05 -0700

    added content in show.ejs, added ejs stylesheet functionality

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ const express = require('express');
 // Set packages
 const app = express();
 
+app.use(express.static('./public'));
+
 // Set locals
 const PORT = process.env.PORT || 3001;
 
 /**
  * Routes
  */
+app.set('view engine', 'ejs');
 
 app.get('/hello', (req, res) => {
   res.send('Hello world');
