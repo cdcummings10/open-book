575a62df5df1fab171b3baabc980e9e91667a136
cdcummings10 <ccummings0913@gmail.com>
2019-09-25 10:30:33 -0700

    added database functionality

---
+++ books.sql
@@ -0,0 +1,15 @@
+DROP DATABASE book_app;
+CREATE DATABASE book_app; 
+
+\c book_app;
+
+DROP TABLE IF EXISTS books;
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  author VARCHAR(255),
+  title VARCHAR(255),
+  isbn VARCHAR(255),
+  image_url VARCHAR(255),
+  description TEXT,
+  bookshelf VARCHAR(255)
+);
